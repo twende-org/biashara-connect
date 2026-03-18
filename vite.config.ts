5e1da015139a798fcd911c7f05f3565b526baa34
FineDr <finesawa36@gmail.com>
2026-03-19 01:14:06 +0300

    add the week changes to the code base and send them live

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+//import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +12,10 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "development" 
+//componentTagger()
+
+].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
